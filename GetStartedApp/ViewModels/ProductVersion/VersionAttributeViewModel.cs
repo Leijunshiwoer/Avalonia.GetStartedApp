ba2479d18f3c511c3e0b0feeffc30a62e9fcf37e
qi.feng <fengergou233@163.com>
2025-07-07 14:45:37 +0800

    优化用户界面和视图模型结构
    - 修改 `UserView.axaml` 中 `IconButton` 的 `CommandParameter` 绑定方式。
    - 简化 `ProductVersion.axaml` 中 `IconButton` 的属性。
    - 新增 `SideMenuView.axaml` 中 `NavMenuItem` 的样式，增强视觉效果。
    - 更新 `README.md`，添加 Avalonia 跨平台开发的介绍和 DeepWiki 链接。
    - 创建 `VersionAttributeViewModel` 类，作为视图模型基础。
    - 在 `VersionAttribute.axaml` 中添加 `UserControl` 的基本结构和注释。
    - 在 `VersionAttribute.axaml.cs` 中初始化 `VersionAttribute` 组件。

---
+++ GetStartedApp/ViewModels/ProductVersion/VersionAttributeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetStartedApp.ViewModels.ProductVersion
+{
+    public class VersionAttributeViewModel:ViewModelBase
+    {
+        public VersionAttributeViewModel()
+        {
+                
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Avalonia 跨平台开发
+
+
+wiki [![Ask DeepWiki](https://deepwiki.com/badge.svg)](https://deepwiki.com/Leijunshiwoer/Avalonia.GetStartedApp)
